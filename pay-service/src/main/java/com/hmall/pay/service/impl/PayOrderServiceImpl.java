1659bcf0d9639ce3da6d36cf83f61ab0e14c0c43
ChiYuouo <zyc630089289@outlook.com>
2026-05-22 20:05:06 +0800

    实现支付pay的分布式事务功能

--- pay-service/src/main/java/com/hmall/pay/service/impl/PayOrderServiceImpl.java
+++ pay-service/src/main/java/com/hmall/pay/service/impl/PayOrderServiceImpl.java
@@ -14,6 +14,7 @@ import com.hmall.pay.domain.po.PayOrder;
 import com.hmall.pay.enums.PayStatus;
 import com.hmall.pay.mapper.PayOrderMapper;
 import com.hmall.pay.service.IPayOrderService;
+import io.seata.spring.annotation.GlobalTransactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -48,7 +49,7 @@ public class PayOrderServiceImpl extends ServiceImpl<PayOrderMapper, PayOrder> i
     }
 
     @Override
-    @Transactional
+    @GlobalTransactional
     public void tryPayOrderByBalance(PayOrderFormDTO payOrderFormDTO) {
         // 1.查询支付单
         PayOrder po = getById(payOrderFormDTO.getId());
@@ -66,6 +67,9 @@ public class PayOrderServiceImpl extends ServiceImpl<PayOrderMapper, PayOrder> i
         }
         // 5.修改订单状态
         tradeClient.markOrderPaySuccess(po.getBizOrderNo());
+
+        //故意制造错误，看是否回滚
+        //int i=1/0;
     }
 
     public boolean markPayOrderSuccess(Long id, LocalDateTime successTime) {
